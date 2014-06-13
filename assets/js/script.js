a703d1cb6d32da49ea81c4b978ec64287219de32
Hugo Marinho <hugoharan@gmail.com>
2014-06-13 20:01:48 -0300

    problemas de atualizao da interface corrigidos

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
 
-	var Cadastro = function ($scope){
+var Database = function ($scope){
 
 
+	var init = function(){
+		var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
+		var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
 
-		var init = function(){
-			$scope.selecao = {id : 0, nome: "", grupo: ""};
-			$scope.lista = [];
-			var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
-		    var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
+		if (!window.indexedDB){
+			alert("Indexed DB não suportado!");
+		}
+		else {
+			var request = window.indexedDB.open("Selecoes", 1);
+			console.log(request);
+			request.onerror = function(e) {
+				alert("Erro na conexão com o banco de dados");
+				console.log(e.target.errorCode);
+			};
+			request.onsuccess = function(e) {
+				db = request.result;
 
-		    if (!window.indexedDB){
-		        alert("Indexed DB não suportado!");
-		    }
-		    else {
-		       	var request = window.indexedDB.open("Selecoes", 1);
-
-				request.onerror = function(e) {
-				  alert("Erro na conexão com o banco de dados");
-				  console.log(e.target.errorCode);
-				};
-				request.onsuccess = function(e) {
-				  db = request.result;
-				  $scope.listar();
+				  //console.log($scope.listar);
+				  //$scope.listar();
+				  listar();
 				};
 				request.onupgradeneeded = function (e) {
 					var thisDb = e.target.result;
@@ -31,64 +31,151 @@
 						store.createIndex("nome", "nome", { unique: true });
 						store.createIndex("grupo", "grupo", { unique: false });
 					}
-		        };
-		    }
-	    }();
+				};
+			}
+		}();
+
+		var salvar = function(callback){
+			var transaction = db.transaction(["selecoes"], "readwrite");
+			var objectStore = transaction.objectStore("selecoes");
+
+			var obj = {};
+			obj.nome = $scope.selecao.nome;
+			obj.grupo = $scope.selecao.grupo;
+
+			if($scope.selecao.id > 0){
+				obj.id = $scope.selecao.id;
+				var request = objectStore.get(obj.id);
+				request.onerror = function(event) {
+					alert("Erro ao buscar selecionado!");
+					console.log(e.target.errorCode);
+				};
+				request.onsuccess = function(event) {
+					var data = request.result;
+					data = obj;
+
+					var requestUpdate = objectStore.put(data);
+					requestUpdate.onerror = function(event) {
+						alert("Erro ao editar selecionado!");
+						console.log(e.target.errorCode);
+					};
+					requestUpdate.onsuccess = function(event) {
+						alert("editado");
+						$scope.$apply(function(){
+							$scope.selecao = {id : 0, nome: "", grupo: ""};
+						});
+					};
+				};
+			}
+			else{
+				var request = objectStore.add(obj);
+
+				request.onsuccess = function (e) {
+					var id = e.srcElement.result;
+					$scope.$apply(function(){
+						$scope.selecao = {id : 0, nome: "", grupo: ""};
+						listar();
+					});
+					if(callback)callback();
+				};
+				request.onerror = function(e) {
+					alert("Erro ao salvar");
+					console.log(e.target.errorCode);
+				};
+			}
 
 
-	    $scope.salvar = function () {
-	        var transaction = db.transaction(["selecoes"], "readwrite");
-	        var objectStore = transaction.objectStore("selecoes");
-	        var request = objectStore.add({ nome: $scope.selecao.nome, grupo: $scope.selecao.grupo});
+		}
 
-	        request.onsuccess = function (e) {
-	            //obj.id = e.srcElement.result;
-	            $scope.listar();
-	        };
-	        request.onerror = function(e) {
-			  alert("Erro ao salvar");
-			  console.log(e.target.errorCode);
-			};
-        }
-
-	    $scope.editar = function (item){
-	    	debugger;
-	    	$scope.selecao = item;
-	    	debugger;
-	    }
-
-	    $scope.excluir = function(item){
-	    	var request = db.transaction(["selecoes"], "readwrite")
-                .objectStore("selecoes")
-                .delete(item.id);
-			request.onsuccess = function(event) {
-			  alert("sucesso!");
-			  var index = $scope.lista.indexOf(item);
-			  $scope.lista.splice(index,1)
-			  return(event);
-			};
-	    }
-
-	    $scope.listar = function(){
+		var listar = function(){
 			var transaction = db.transaction(["selecoes"]);
-	        var objectStore = transaction.objectStore("selecoes");
+			var objectStore = transaction.objectStore("selecoes");
 			var selecoes = [];
-	        var request = objectStore.openCursor();
-	        request.onsuccess = function(e) {
-	            var cursor = e.target.result;
-	            if (cursor) {
-	               $scope.lista.unshift(cursor.value);
-	                cursor.continue();
-	            }
-	            else
-	            	return $scope.lista;
-	        };
+			var request = objectStore.openCursor();
+			console.log(request);
+			request.onsuccess = function(e) {
+				var cursor = e.target.result;
+				if (cursor) {
+					$scope.$apply(function(){
+						$scope.selecoes.push(cursor.value);
+						cursor.continue();
+					});
 
-	        request.onerror = function(e) {
+				}
+				else
+					return $scope.selecoes;
+			};
+
+			request.onerror = function(e) {
 				alert("Erro ao listar seleções");
 				console.log(e.target.errorCode);
 			};
+		}
+
+		var excluir = function(item, callback){
+			var transaction = db.transaction(["selecoes"], "readwrite")
+				.objectStore("selecoes")
+				.delete(item.id);
+			transaction.onsuccess = function(e) {
+				if(callback)callback();
+				$scope.$apply(function(){
+					var index = $scope.selecoes.indexOf(item);
+			  		$scope.selecoes.splice(index,1);
+				});
+			};
+		}
+
+		return {
+			salvar : salvar,
+			listar : listar,
+			excluir : excluir
+		}
+	}
+
+
+
+
+	var Cadastro = function ($scope){
+		var database = new Database($scope);
+
+		var init = function(){
+			$scope.selecao = {id : 0, nome: "", grupo: ""};
+			$scope.selecoes = [];
+
+		};
+
+
+		$scope.salvar = function () {
+	    	// var obj = $scope.selecao;
+	    	// database.salvar(obj);
+	    	database.salvar(saved);
 	    }
 
-	    //init();
-	}//);
+	    $scope.editar = function (item){
+	    	$scope.selecao = item;
+	    	//database.salvar(saved);
+	    }
+
+	    $scope.excluir = function(item){
+	    	var obj = item;
+	    	database.excluir(obj,excluded);
+	    }
+
+	    $scope.listar = function(){
+
+	    }
+
+	    var excluded = function(){
+		  	alert("excluiu");
+		  }
+
+		  var saved = function(){
+		  	alert("salvou!");
+		  }
+
+		  var edited = function(){
+		  	alert("editou");
+		  }
+
+		  init();
+		}
