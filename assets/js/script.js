990a5bc43035ec45e04f773fd02769c425d1d1da
Hugo Marinho <hugoharan@gmail.com>
2014-06-12 22:51:40 -0300

    criando banco de dados no IndexedDB e salvando model

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,54 @@
-$(document).ready(function(){
+	var Cadastro = function ($scope){
 
-});
+		$scope.selecao = {id : 0, nome: "", grupo: ""};
+		$scope.lista = [];
+
+		var init = function(){
+
+			var indexedDB = window.indexedDB || window.webkitIndexedDB || window.mozIndexedDB || window.msIndexedDB;
+		    var IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction;
+
+		    if (!window.indexedDB){
+		        alert("Indexed DB não suportado!");
+		    }
+		    else {
+		       	var request = window.indexedDB.open("Selecoes", 1);
+
+				request.onerror = function(e) {
+				  alert("Erro na conexão com o banco de dados");
+				  console.log(e.target.errorCode);
+				};
+				request.onsuccess = function(e) {
+				  db = request.result;
+				  $scope.listar();
+				};
+				request.onupgradeneeded = function (e) {
+					var thisDb = e.target.result;
+
+					if(!thisDb.objectStoreNames.contains("selecoes")) {
+						var store = e.target.result.createObjectStore(["selecoes"], { keyPath: "id", autoIncrement: true });
+						store.createIndex("nome", "nome", { unique: true });
+						store.createIndex("grupo", "grupo", { unique: false });
+					}
+		        };
+		    }
+	    }();
+
+
+	    $scope.salvar = function () {
+	        var transaction = db.transaction(["selecoes"], "readwrite");
+	        var objectStore = transaction.objectStore("selecoes");
+	        var request = objectStore.add({ nome: $scope.selecao.nome, grupo: $scope.selecao.grupo});
+
+	        request.onsuccess = function (e) {
+	            //obj.id = e.srcElement.result;
+	            $scope.listar();
+	        };
+	        request.onerror = function(e) {
+			  alert("Erro ao salvar");
+			  console.log(e.target.errorCode);
+			};
+        }
+
+
+	}
